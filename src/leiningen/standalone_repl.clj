cee12c325098ec938ba2312d17a437458bab010e
Phil Hagelberg <technomancy@gmail.com>
2013-04-04 11:48:15 -0700

    That's what we get for relying on a private var.

--- src/leiningen/standalone_repl.clj
+++ src/leiningen/standalone_repl.clj
@@ -1,19 +1,12 @@
 (ns leiningen.standalone-repl
   (:require [leiningen.trampoline :as trampoline]
             [leiningen.repl :as repl]
-            [leiningen.core.eval :as eval]
-            [leiningen.core.project :as project]
-            [leiningen.core.user :as user]))
-
-(defn- trampoline-repl [project]
-  (let [profiles (repl/profiles-for project true true)]
-    (eval/eval-in-project
-     (project/merge-profiles project profiles)
-     `(reply.main/launch-standalone ~(repl/options-for-reply project))
-     `(require ~@(#'leiningen.repl/init-requires project 'reply.main)))))
+            [leiningen.run :as run]
+            [leiningen.core.project :as project]))
 
 (defn standalone-repl
   "Backport of a bugfix for trampoline repl."
   [project]
-  (with-redefs [leiningen.repl/trampoline-repl trampoline-repl]
-    (trampoline/trampoline project "repl")))
+  (trampoline/trampoline (project/merge-profiles project
+                                                 [repl/trampoline-profile])
+                         "run" "-m" "reply.main/launch-standalone" "nil"))
